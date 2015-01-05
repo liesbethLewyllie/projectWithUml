162a5005c61df6fcb370c5a44dd7d6b269ba8720
liesbethLewyllie <liesbeth.lewyllie@hogent.be>
2015-01-05 15:39:53 +0100

    Merge origin/master
    Conflicts:
    	src/ui/ThermometerApplicatie.java

--- src/ui/ThermometerApplicatie.java
+++ src/ui/ThermometerApplicatie.java
@@ -27,6 +27,7 @@ public class ThermometerApplicatie {
         System.out.println("Test Liesbeth5");
         System.out.println("Test Liesbeth6");
         System.out.println("Test Liesbeth7");
+         System.out.println("Test na global aanpassing enter");
 
     }
     public void test(){
