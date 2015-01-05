3579aebe889e6eda8fec05d34b1abc5e3b2cc216
liesbethLewyllie <liesbeth.lewyllie@hogent.be>
2015-01-05 14:48:21 +0100

    Merge origin/master
    Conflicts:
    	src/ui/ThermometerApplicatie.java

--- src/ui/ThermometerApplicatie.java
+++ src/ui/ThermometerApplicatie.java
@@ -25,32 +25,4 @@ public class ThermometerApplicatie {
             System.out.println("Test Liesbeth7");
             System.out.println("toevoegen lijn nadat extra lijn al online stond");
     }
-package ui;
-
-import domein.DomeinController;
-
-public class ThermometerApplicatie {
- 
-    public static void main(String[] args){
-        
-        DomeinController dc = new DomeinController();
-
-        dc.initialiseerThermometer();
-        
-        System.out.printf("De huidige temperatuur: %.2f graden Celcius%n", dc.bepaalTemp(1));
-        System.out.printf("De huidige temperatuur: %.2f graden Fahrenheit%n", dc.bepaalTemp(2));
-        System.out.printf("De huidige temperatuur: %.2f Kelvin%n", dc.bepaalTemp(3));
-        
-        System.out.println("Test Liesbeth na wijziging Sonia");
-        System.out.println("Test Liesbeth");
-        System.out.println("Test Liesbeth2");
-     
-        System.out.println("Test Liesbeth3test");
-        System.out.println("Test Liesbeth4");
-         System.out.println("Test Liesbeth5");
-         System.out.println("Test Liesbeth6");
-            System.out.println("Test Liesbeth7");
-              System.out.println("Test Liesbeth8");
-        
-    }
 }
