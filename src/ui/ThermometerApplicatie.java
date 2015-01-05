25b96665860ddb9ef9e7fab822d413e28f16ecc8
liesbethLewyllie <liesbeth.lewyllie@hogent.be>
2015-01-05 14:43:47 +0100

    extra lijn met al extra lijn online

--- src/ui/ThermometerApplicatie.java
+++ src/ui/ThermometerApplicatie.java
@@ -23,6 +23,6 @@ public class ThermometerApplicatie {
          System.out.println("Test Liesbeth5");
          System.out.println("Test Liesbeth6");
             System.out.println("Test Liesbeth7");
-        
+            System.out.println("toevoegen lijn nadat extra lijn al online stond");
     }
 }
