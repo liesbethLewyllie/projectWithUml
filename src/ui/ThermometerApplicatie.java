d0c3415f2e072d43a2f81dcfb6562935e3cdd4e0
liesbethLewyllie <liesbeth.lewyllie@hogent.be>
2015-01-05 15:09:26 +0100

    Merge origin/master
    Conflicts:
    	src/ui/ThermometerApplicatie.java

--- src/ui/ThermometerApplicatie.java
+++ src/ui/ThermometerApplicatie.java
@@ -3,29 +3,32 @@ package ui;
 import domein.DomeinController;
 
 public class ThermometerApplicatie {
- 
-    public static void main(String[] args){
-        
-        DomeinController dc = new DomeinController();
+
+    private DomeinController dc = new DomeinController();
+
+    public static void main(String[] args) {
+        new ThermometerApplicatie().run();
+    }
+
+    public void run() {
 
         dc.initialiseerThermometer();
-        
+
         System.out.printf("De huidige temperatuur: %.2f graden Celcius%n", dc.bepaalTemp(1));
         System.out.printf("De huidige temperatuur: %.2f graden Fahrenheit%n", dc.bepaalTemp(2));
         System.out.printf("De huidige temperatuur: %.2f Kelvin%n", dc.bepaalTemp(3));
-        
+
         System.out.println("Test Liesbeth na wijziging Sonia");
         System.out.println("Test Liesbeth");
         System.out.println("Test Liesbeth2");
-     
+
         System.out.println("Test Liesbeth3test");
         System.out.println("Test Liesbeth4");
-         System.out.println("Test Liesbeth5");
-         System.out.println("Test Liesbeth6");
-            System.out.println("Test Liesbeth7");
-            System.out.println("toevoegen lijn nadat extra lijn al online stond");
+        System.out.println("Test Liesbeth5");
+        System.out.println("Test Liesbeth6");
+        System.out.println("Test Liesbeth7");
+
     }
-    
     public void test(){
         String test = null;
         test = new String();
