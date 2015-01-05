5618d4afb4425e22212aad40c489c71607094313
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2015-01-05 15:40:09 +0100

    oeps, ; vergeten

--- src/ui/ThermometerApplicatie.java
+++ src/ui/ThermometerApplicatie.java
@@ -28,7 +28,7 @@ public class ThermometerApplicatie {
         System.out.println("Test Liesbeth6");
         System.out.println("Test Liesbeth7");
 
-        System.out.println("Test Bert")
+        System.out.println("Test Bert");
     }
     public void test(){
         String test = null;
